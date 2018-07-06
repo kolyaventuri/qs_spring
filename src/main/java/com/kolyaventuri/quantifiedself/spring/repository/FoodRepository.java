7ddd5ad85508069ce2f0e800c7c37b23d307fefc
Kolya Venturi <kolya.venturi@enx3s.com>
2018-07-06 13:28:57 -0600

    Add FoodRepository

--- src/main/java/com/kolyaventuri/quantifiedself/spring/repository/FoodRepository.java
+++ src/main/java/com/kolyaventuri/quantifiedself/spring/repository/FoodRepository.java
@@ -1,5 +1,10 @@
 package com.kolyaventuri.quantifiedself.spring.repository;
 
-public class FoodRepository {
+import com.kolyaventuri.quantifiedself.spring.model.Food;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FoodRepository extends JpaRepository<Food, Long> {
 }
