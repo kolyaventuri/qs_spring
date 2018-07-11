2ee5c599b8b1b1344dc689d3955d31a010b1816d
Kolya Venturi <kolya.venturi@enx3s.com>
2018-07-11 14:29:50 -0600

    Post endpoint to create foods

--- src/main/java/com/kolyaventuri/quantifiedself/spring/controllers/FoodsController.java
+++ src/main/java/com/kolyaventuri/quantifiedself/spring/controllers/FoodsController.java
@@ -1,9 +1,12 @@
 package com.kolyaventuri.quantifiedself.spring.controllers;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kolyaventuri.quantifiedself.spring.model.Food;
@@ -19,5 +22,18 @@ public class FoodsController {
 	@RequestMapping("")
 	public List<Food> index() {
 		return foodRepository.findAll();
+	}
+	
+	@RequestMapping(value="", method=RequestMethod.POST)
+	public Food create(@RequestBody Map<String, Object> payload) {
+		Map<String, Object> foodObject = (Map<String, Object>) payload.get("food");
+		
+		String name = (String) foodObject.get("name");
+		int calories = (int) foodObject.get("calories");
+				
+		Food food = new Food(name, calories);
+		foodRepository.save(food);
+		
+		return food;
 	}
 }
