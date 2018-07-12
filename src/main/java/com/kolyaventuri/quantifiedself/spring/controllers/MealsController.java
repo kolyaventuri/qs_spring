bfb2f677b956ae27f6274fc4ee2a4cc12b7a060d
Kolya Venturi <kolya.venturi@enx3s.com>
2018-07-11 21:35:52 -0600

    meal show endpoint

--- src/main/java/com/kolyaventuri/quantifiedself/spring/controllers/MealsController.java
+++ src/main/java/com/kolyaventuri/quantifiedself/spring/controllers/MealsController.java
@@ -1,8 +1,13 @@
 package com.kolyaventuri.quantifiedself.spring.controllers;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +24,19 @@ public class MealsController {
 	@RequestMapping("")
 	public List<Meal> index() {
 		return mealRepository.findAll();
+	}
+	
+	@RequestMapping("/{id}")
+	public Meal show(@PathVariable long id, HttpServletResponse response) throws IOException {
+		Optional<Meal> foundMeal = mealRepository.findById(id);
+		
+		if(!foundMeal.isPresent()) {
+			response.sendError(404);
+			return null;
+		}
+		
+		Meal meal = foundMeal.get();
+		
+		return meal;
 	}
 }
